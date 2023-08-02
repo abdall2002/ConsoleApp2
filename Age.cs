56a1952920d1680dadf1c94543c502aea60e4e4c
unknown <abdallahaliahmed517@gmail.com>
2023-08-02 16:09:50 +0300

    add Age

---
+++ Age.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedType
+{
+    readonly struct Age
+    {
+        //public int years;                    // -> must br all contains readonly;
+        public readonly int years;
+        public readonly int Whatever; 
+        
+        // Constructor;
+        public Age(int years, int whatever)
+        {
+            this.years = years;
+            this.Whatever = whatever;
+        }
+    }
+}
