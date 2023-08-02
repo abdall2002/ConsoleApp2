75c11ba7a2b7fe4dccd8aa1594adbbeafff4dd6f
unknown <abdallahaliahmed517@gmail.com>
2023-08-02 13:41:24 +0300

    add Student

---
+++ Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedType
+{
+    struct Student
+    {
+        public Student(string name)
+        {
+            
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("This is Student");
+        }
+
+
+    }
+}
