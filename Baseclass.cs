d65b198c1f77be889a6c9955de2fdebd5f19a9f8
unknown <abdallahaliahmed517@gmail.com>
2023-08-05 15:38:12 +0300

    add Baseclass

---
+++ Baseclass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedType
+{
+    class Baseclass
+    {
+        public int x;
+
+        public Baseclass()
+        {
+            
+        }
+
+        public Baseclass(int value)
+        {
+            x = value;
+        }   
+    }
+}
