5e1e380741f85ac0db049190342d1dc39f99e523
unknown <abdallahaliahmed517@gmail.com>
2023-08-05 16:12:22 +0300

    add IDrivable

--- Caterpiller.cs
+++ Caterpiller.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace NestedType
 {
-    class Caterpiller : Vehicle
+    class Caterpiller : Vehicle, ILoader
     {
         public Caterpiller(string brand, string model, int year) : base(brand, model, year)
         {
             
+        }
+
+        public void Load()
+        {
+            Console.WriteLine("Loading");    
+        }
+
+        public void UnLoad()
+        {
+            Console.WriteLine("UnLoading");
         }
     }
 }
---
+++ IDrivablle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedType
+{
+    interface IDrivablle
+    {
+        void Move();
+        void Stop();
+    }
+}
